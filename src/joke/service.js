c04240ed804383bd2d61cff07b44745a5faeed3c
jakubk3697 <kupson3697@gmail.com>
2020-09-16 23:07:48 +0200

    changed API functions to async/await

--- src/joke/service.js
+++ src/joke/service.js
@@ -1,14 +1,31 @@
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable require-jsdoc */
 // /* eslint-disable require-jsdoc */
-export default () => {
-  return fetch("https://official-joke-api.appspot.com/random_joke")
-    .then((res) => res.json())
-    .then((data) => {
-      if (data.type === "programming") {
-        return data;
-      } else {
-        return fetch("http://api.icndb.com/jokes/random")
-          .then((res) => res.json())
-          .then((data) => data);
-      }
-    });
-};
+
+/* v.1 */
+export default async function getJoke() {
+  const res = await fetch("https://official-joke-api.appspot.com/random_joke");
+  const data = await res.json();
+  if (data.type === "general") {
+    return data;
+  } else {
+    return fetch("http://api.icndb.com/jokes/random")
+      .then((res) => res.json())
+      .then((data) => data);
+  }
+}
+
+/* v.2 */
+// export default () => {
+//   return fetch("https://official-joke-api.appspot.com/random_joke")
+//     .then((res) => res.json())
+//     .then((data) => {
+//       if (data.type === "programming") {
+//         return data;
+//       } else {
+//         return fetch("http://api.icndb.com/jokes/random")
+//           .then((res) => res.json())
+//           .then((data) => data);
+//       }
+//     });
+// };
--- src/joke/show.js
+++ src/joke/show.js
@@ -1,13 +1,12 @@
 /* eslint-disable require-jsdoc */
 import getJoke from "./service.js";
 
-export default () => {
-  getJoke().then((data) => {
-    if (data.setup) {
-      alert(data.setup);
-      alert(data.punchline);
-    } else {
-      alert(data.value.joke);
-    }
-  });
-};
+export default async function() {
+  const data = await getJoke();
+  if (data.setup) {
+    alert(data.setup);
+    alert(data.punchline);
+  } else {
+    alert(data.value.joke);
+  }
+}
