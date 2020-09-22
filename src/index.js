df1f5b1fcc9119d8b30bf23b1998a020865b5e50
jakubk3697 <kupson3697@gmail.com>
2020-09-22 17:52:18 +0200

    new features for markdown

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 /* eslint-disable no-unused-vars */
 import initMd from "markdown-element";
 // import main from "./github/index.js";
-import { getBlogPost } from "./github/service.js";
+import init from "./blog/index.js";
 
-getBlogPost().then((blogPost) => {
-  const md = document.createElement("mark-down");
-  md.textContent = blogPost;
-  document.body.appendChild(md);
-});
+init();
+
 // import getJoke from "./joke/index.js";
 // import game from "./game/index.js";
 // getJoke();
