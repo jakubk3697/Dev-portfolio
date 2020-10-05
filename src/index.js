3548b0d48ce53678a69d1f5f2249d43884966a83
jakubk3697 <kupson3697@gmail.com>
2020-10-05 19:43:37 +0200

    Added clean and cp[y webpack plugin. Created HTML structure.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 /* eslint-disable no-unused-vars */
 import initMd from "markdown-element";
-// import main from "./github/index.js";
-// import init from "./blog/index.js";
-
-// init();
-
+import main from "./github/index.js";
+// import initPost from "./blog/index.js";
+import showAboutMe from "./about/index.js";
 // import getJoke from "./joke/index.js";
 // import game from "./game/index.js";
-// getJoke();
 // main();
-// game();
-
-import showAboutMe from "./about/index.js";
-
+// initPost();
 showAboutMe();
+// getJoke();
+// game();
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
-var HtmlWebpackPlugin = require("html-webpack-plugin");
+/* eslint-disable no-var */
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   mode: "production",
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: "src/index.html",
+    }),
+    new CopyPlugin({
+      patterns: [{ from: "src/img/", to: "img/" }],
     }),
   ],
 };
